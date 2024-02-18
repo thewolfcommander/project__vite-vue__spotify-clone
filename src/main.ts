6358f3545d1115e15ecfef73c7a8e27d0ada34ee
Manoj Pawan Tyagi <tyagimanojtyagi.22@gmail.com>
2024-02-18 18:28:29 +0530

    chore: configure pinia-persisted-state

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,18 @@
-import './assets/main.css'
+import "./assets/main.css";
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
 
-import App from './App.vue'
-import router from './router'
+import App from "./App.vue";
+import router from "./router";
 
-const app = createApp(App)
+import piniaPluginPersistedState from "pinia-plugin-persistedstate";
 
-app.use(createPinia())
-app.use(router)
+const app = createApp(App);
+const pinia = createPinia();
+pinia.use(piniaPluginPersistedState);
 
-app.mount('#app')
+app.use(pinia);
+app.use(router);
+
+app.mount("#app");
